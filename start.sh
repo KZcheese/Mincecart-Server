db56cc4b94cea45013d656d5ec43df8aae1c6974
Kevin Zhan <kevin.w.zhan@gmail.com>
2025-01-15 00:53:17 -0500

    post james unmcafee

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -Xmx4G -jar fabric-server-mc.1.20.1-loader.0.16.10-launcher.1.0.1.jar nogui
+java -Xms512M -Xmx4G -XX:+UseG1GC -jar fabric-server-mc.1.20.1-loader.0.16.10-launcher.1.0.1.jar nogui
